e9c80d8bbcffb37a9d02e733846e354d789ae377
Powis Forjoe <pforjoe@ups.com>
2021-10-25 21:09:27 -0400

    Update datamanger.py

--- AssetAllocation/datamanger/datamanger.py
+++ AssetAllocation/datamanger/datamanger.py
@@ -19,7 +19,7 @@ CWD = os.getcwd()
 DATA_FP = CWD + '\\data\\'
 MV_INPUTS_FP = DATA_FP + 'mv_inputs\\'
 TS_FP = DATA_FP + 'time_series\\'
-OUTPUTS_FP = DATA_FP + 'outputs\\'
+PLAN_INPUTS_FP = DATA_FP + 'plan_inputs\\'
 
 
 def get_fi_data(filename):
@@ -63,7 +63,7 @@ def get_rv_data(filename):
         rv_df['12mo expiry'] / rv_df['3mo expiry']
     return rv_df
 
-def get_mv_inputs_data(filename):
+def get_mv_inputs_data(filename, plan='IBT'):
     
     filepath = MV_INPUTS_FP + filename
     
@@ -81,7 +81,7 @@ def get_mv_inputs_data(filename):
     rv_data = get_rv_data(filepath)
 
     # Get weights
-    weights = pd.read_excel(filepath, sheet_name='weights', index_col=0)
+    weights = get_weights(plan=plan)
 
     # get vol definitions
     vol_defs = pd.read_excel(filepath, sheet_name='vol_defs')
@@ -105,17 +105,17 @@ def get_mkt_factor_premiums(filename):
 def merge_dfs(main_df, new_df):
     return pd.merge(main_df, new_df,left_index=True, right_index=True, how='outer')
 
-def get_output_data(filename):
+def get_plan_data(filename):
     
-    output_list = ['ret_vol', 'corr', 'weights']
-    output_dict = {}
-    for sheet in output_list:
-        output_dict[sheet] = pd.read_excel(OUTPUTS_FP+filename,
+    plan_list = ['ret_vol', 'corr', 'weights']
+    plan_dict = {}
+    for sheet in plan_list:
+        plan_dict[sheet] = pd.read_excel(PLAN_INPUTS_FP+filename,
                                            sheet_name=sheet,
                                            index_col=0)
-    return output_dict
+    return plan_dict
 
-def get_returns_df(filename, year='2011'):
+def get_returns_df(plan='IBT', year='2011'):
     """
 
 
@@ -132,32 +132,61 @@ def get_returns_df(filename, year='2011'):
         DESCRIPTION.
 
     """
-    returns_df = pd.read_excel(filename,
+    asset_ret_df = get_asset_returns(year=year)
+    liab_ret_df = get_liab_returns(plan=plan)
+    returns_df = merge_dfs(liab_ret_df, asset_ret_df)
+    return returns_df
+
+def get_asset_returns(filename='return_data.xlsx', year='2011'):
+    """
+
+
+    Parameters
+    ----------
+    filename : TYPE
+        DESCRIPTION.
+    year : TYPE, optional
+        DESCRIPTION. The default is '2011'.
+
+    Returns
+    -------
+    returns_df : TYPE
+        DESCRIPTION.
+
+    """
+    filepath = TS_FP+filename
+    asset_ret_df = pd.read_excel(filepath,
                              sheet_name=year, index_col=0)
     # returns_df['Credit'] = 0.2*returns_df['CS LL'] + 0.3*returns_df['BOA HY'] + 0.5*returns_df['CDLI']
     # returns_df['Liquid Alternatives'] = 0.33*returns_df['HF MACRO'] + 0.33*returns_df['HFRI MACRO'] + 0.34*returns_df['TREND']
-    returns_df = returns_df[['Liability', '15+ STRIPS', 'Long Corps', 'ULTRA 30Y FUTURES', 'Total EQ Unhedged', 'Total Liquid Alts',
+    asset_ret_df = asset_ret_df[['15+ STRIPS', 'Long Corps', 'ULTRA 30Y FUTURES', 'Total EQ Unhedged', 'Total Liquid Alts',
                              'Total Private Equity', 'Total Credit', 'Total Real Estate', 'Cash', 'Equity Hedges']]
-    returns_df.columns = ['Liability', '15+ STRIPS', 'Long Corporate','Ultra 30-Year UST Futures', 'Equity', 'Liquid Alternatives',
+    asset_ret_df.columns = ['15+ STRIPS', 'Long Corporate','Ultra 30-Year UST Futures', 'Equity', 'Liquid Alternatives',
                           'Private Equity', 'Credit', 'Real Estate', 'Cash', 'Equity Hedges']
-    returns_df.dropna(inplace=True)
-    return returns_df
+    asset_ret_df.dropna(inplace=True)
+    return asset_ret_df
 
-def get_weights(filename = 'weights.xlsx'):
-    weights_df = pd.read_excel(filename, sheet_name='weights',index_col=0)
+def get_liab_returns(filename='liability_return_data.xlsx', plan='IBT'):
+    filepath = TS_FP+filename
+    liab_ret_df = pd.read_excel(filepath, sheet_name=plan, usecols=[0,1], index_col=0)
+    return liab_ret_df
+    
+def get_weights(filename = 'weights.xlsx', plan='IBT'):
+    filepath = PLAN_INPUTS_FP+filename
+    weights_df = pd.read_excel(filepath, sheet_name=plan,index_col=0)
     weights_df['FS AdjWeights'] = weights_df['Weights'] * weights_df['Factor Loadings']
     # weights_df = weights_df[['FS AdjWeights']]
     return weights_df
 
-def get_ts_data(filename, year='2011'):
-    filepath = TS_FP + filename
-    returns_df = get_returns_df(filepath, year)
-    weights_df = get_weights(filepath)
+def get_ts_data(plan='IBT', year='2011'):
+    returns_df = get_returns_df(plan=plan, year=year)
+    weights_df = get_weights(plan=plan)
     return {'returns': returns_df,
             'weights': weights_df}
 
-def get_bounds(filename):
-    bnds = pd.read_excel(filename, sheet_name='bnds', index_col=0)
+def get_bounds(filename='bounds.xlsx', plan='IBT'):
+    filepath=PLAN_INPUTS_FP+filename
+    bnds = pd.read_excel(filepath, sheet_name=plan, index_col=0)
     return tuple(zip(bnds.Lower, bnds.Upper))
 
 def get_ports_df(rets, vols, weights, symbols, raw=True):
