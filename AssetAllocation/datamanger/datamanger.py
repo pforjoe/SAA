df26d8b8b56b7b231048cbacab9a5c72863f025d
Powis Forjoe <pforjoe@ups.com>
2021-10-30 22:58:30 -0400

    Update historical returns of credit, equity and RE

--- AssetAllocation/datamanger/datamanger.py
+++ AssetAllocation/datamanger/datamanger.py
@@ -169,7 +169,7 @@ def get_asset_returns(filename='return_data.xlsx', year='2010'):
                              sheet_name=year, index_col=0)
     # returns_df['Credit'] = 0.2*returns_df['CS LL'] + 0.3*returns_df['BOA HY'] + 0.5*returns_df['CDLI']
     # returns_df['Liquid Alternatives'] = 0.33*returns_df['HF MACRO'] + 0.33*returns_df['HFRI MACRO'] + 0.34*returns_df['TREND']
-    asset_ret_df = asset_ret_df[['15+ STRIPS', 'Long Corps', 'WN1 COMB Comdty', 'Total EQ Unhedged', 'Total Liquid Alts',
+    asset_ret_df = asset_ret_df[['15+ STRIPS', 'Long Corps', 'WN1 COMB Comdty', 'Total Dom Eq w/o Derivatives', 'Total Liquid Alts',
                              'Total Private Equity', 'Total Credit', 'Total Real Estate', 'Cash', 'Equity Hedges']]
     asset_ret_df.columns = ['15+ STRIPS', 'Long Corporate','Ultra 30Y Futures', 'Equity', 'Liquid Alternatives',
                           'Private Equity', 'Credit', 'Real Estate', 'Cash', 'Hedges']
