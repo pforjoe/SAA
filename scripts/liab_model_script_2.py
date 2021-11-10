603bf7a9e27b86db15f5175ef830c1e6ebda04f2
Powis Forjoe <pforjoe@ups.com>
2021-11-10 13:59:20 -0500

    update liability model

--- scripts/liab_model_script_2.py
+++ scripts/liab_model_script_2.py
@@ -31,13 +31,20 @@ def frange(start, stop, step):
 def set_cfs_time_col(df_cfs):
     df_cfs['Time'] = list(frange(1/12, (len(df_cfs)+.9)/12, 1/12))
 
-def get_cf_data(cf_type='PBO'):
+def get_cf_data(cf_type='PBO',plan='IBT'):
     df_cfs = pd.read_excel(dm.TS_FP+'annual_cashflows_data.xlsx', sheet_name='PBO', index_col=0)/12
     df_cfs = dm.reindex_to_monthly_data(df_cfs)
     temp_cfs = pd.read_excel(dm.TS_FP+'monthly_cashflows_data.xlsx', sheet_name='PBO', index_col=0)
     df_cfs = temp_cfs.append(df_cfs)
     set_cfs_time_col(df_cfs)
     return df_cfs
+
+def get_ftse_data():
+    df_old_ftse = pd.read_excel(dm.TS_FP+'ftse_data.xlsx',sheet_name='old_data', index_col=0)
+    df_new_ftse = pd.read_excel(dm.TS_FP+'ftse_data.xlsx',sheet_name='new_data', index_col=0)
+    df_ftse = dm.merge_dfs(df_new_ftse,df_old_ftse)
+    df_ftse.reset_index(inplace=True)
+    return df_ftse
 
 def generate_liab_curve(df_ftse, cfs):
     liab_curve_dict = {}
@@ -99,28 +106,63 @@ def compute_irr(cfs,dfs,df_pv_dr):
         irr_list[j] += irr(cashflows, yrs, .03)
     return irr_list
 
+def full_solve(initial_mv, df_dr, cfs,dfs,yrs_to_ff, ff_ratio,x0):
+    return np.asscalar(fsolve(fullfill_solve, 
+                              x0=x0, 
+                              args=(initial_mv, df_dr, cfs,dfs,yrs_to_ff, ff_ratio)))
+
+
+def fullfill_solve(fullfill_return, initial_mv, df_dr, cfs,dfs,yrs_to_ff, ff_ratio):
+    #fullfillment_return = .045
+    # MV_Assets = 13203547000
+    pv_df_erf = np.zeros(len(dfs))
+    asset_mv = np.zeros(len(dfs))
+    x = yrs_to_ff/dfs[0]
+    x = x.astype(int)
+    
+    for j in range(len(dfs)):
+        if (j == 0):
+            for i in range(j,len(cfs)):
+                pv_df_erf[j] += (cfs[i]/((1+df_dr['IRR'][-1])**dfs[i-j]))
+                asset_mv[j] = initial_mv
+    
+        else:
+            for i in range(j,len(cfs)):
+                pv_df_erf[j] += (cfs[i]/((1+df_dr['IRR'][-1])**dfs[i-j]))
+                asset_mv[j] = (asset_mv[j-1]*(1+fullfill_return)**dfs[0].tolist())-cfs[j-1]
+    
+    return asset_mv[x] - pv_df_erf[x]*ff_ratio
 
 ############################################################################################################################################################
-# IMPORT CASHFLOWS                                                            
+# IMPORT PBO CASHFLOWS                                                            
 ############################################################################################################################################################
 df_pbo_cfs = get_cf_data('PBO')
 pbo_cfs = np.array(df_pbo_cfs[PLAN])
 dfs = np.array(df_pbo_cfs['Time'])
 
 ############################################################################################################################################################
-df_ftse = pd.read_excel(dm.TS_FP+'ftse_data.xlsx',sheet_name='data')
-liab_curve = generate_liab_curve(df_ftse)
+# IMPORT FTSE CURVES AND INTERPOLATE                                                            
+############################################################################################################################################################
+df_ftse = get_ftse_data()
 
+liab_curve = generate_liab_curve(df_ftse, pbo_cfs)
+
+############################################################################################################################################################
+# COMPUTE PVS && LIAB RET                                                            
 ############################################################################################################################################################
 df_pv = compute_pvs(pbo_cfs, dfs, liab_curve)
 liab_ret_df = compute_liab_ret(df_pv)
 liab_ret_df = dm.merge_dfs(liab_ret_df, df_pv)
 
 ############################################################################################################################################################
+# IMPORT SC CASHFLOWS                                                            
+############################################################################################################################################################
 df_sc_cfs = get_cf_data('SC')
 sc_cfs = np.array(df_sc_cfs[PLAN])
 
 total_cfs = UPS_Contr_Pctg*sc_cfs + pbo_cfs
+############################################################################################################################################################
+# COMPUTE PVS && LIAB RET                                                            
 ############################################################################################################################################################
 df_total_pv = compute_pvs(total_cfs, dfs, liab_curve)
 total_liab_ret_df = compute_liab_ret(df_total_pv)
@@ -130,6 +172,6 @@ df_dr = pd.read_excel(dm.TS_FP+"discount_rate_data.xlsx",sheet_name=PLAN ,index_
 df_pv_dr = compute_pvs_w_dr(pbo_cfs, dfs, df_dr)
 ############################################################################################################################################################
 irr_list = compute_irr(pbo_cfs, dfs, df_pv_dr)
-irr_list_2 = compute_irr(pbo_cfs, dfs, df_total_pv)
 ############################################################################################################################################################
 
+fullfill_irr = full_solve(13203547000,df_dr,pbo_cfs,dfs,20,1.05,.01)
