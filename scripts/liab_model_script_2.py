25242ad8ac0db9ef82d84fedc67ee0083bafd526
pforjoeUPS <pforjoe@ups.com>
2022-12-02 22:55:26 -0500

    Update liab_model_script_2.py

--- scripts/liab_model_script_2.py
+++ scripts/liab_model_script_2.py
@@ -8,96 +8,38 @@ Created on Wed Nov  3 21:05:08 2021
 ############################################################################################################################################################
 # IMPORT LIBRARIES                                                            
 ############################################################################################################################################################
-from AssetAllocation.datamanger import datamanger as dm
+from AssetAllocation.datamanager import datamanager as dm
 from AssetAllocation.analytics.liability_model import liabilityModel
-import pandas as pd
-import numpy as np
-from scipy.optimize import fsolve
 
 ############################################################################################################################################################
-# FUNCTIONS FOR COMPUTING LIAB RETURN                                                         
+# IMPORT CASHFLOW & FTSE DATA REQUIRED FOR CREATING LIABILITY DATA                                                          
 ############################################################################################################################################################
-def compute_pvs(pbo_cashflows, disc_factors, liab_curve=pd.DataFrame,disc_rates=pd.DataFrame):
-    if disc_rates.empty:
-        pv_dict={}
-        for col in liab_curve.columns:
-            temp_pv = 0
-            for j in range (0,len(pbo_cashflows)):
-                temp_pv += (pbo_cashflows[j]/((1+liab_curve[col][j]/100)**disc_factors[j]))
-            pv_dict[col] = temp_pv
-        return pd.DataFrame(pv_dict, index = ['Present Value']).transpose()
-    else:
-        disc_rates_pv_array = np.zeros(len(disc_rates))
-        for i in range(len(disc_rates)):
-            for j in range (0,len(pbo_cashflows)):
-                disc_rates_pv_array[i] += (pbo_cashflows[j]/((1+disc_rates['IRR'][i])**disc_factors[j]))
-            
-        return pd.DataFrame(disc_rates_pv_array, columns=['Present Value'], index=disc_rates.index)
-    
-def npv(irr, cfs, yrs):  
-    return np.sum(cfs / (1. + irr) ** yrs)
-
-def irr(cfs, yrs, x0, **kwargs):
-    return np.asscalar(fsolve(npv, x0=x0,args=(cfs,yrs), **kwargs))
-
-def compute_irr(present_values,pbo_cashflows, disc_factors):
-    irr_array = np.zeros(len(present_values))
-    for j in range (len(present_values)):
-        cashflows = np.append(np.negative(present_values['Present Value'][j]),pbo_cashflows)
-        yrs = np.append(0, disc_factors)
-        irr_array[j] += irr(cashflows, yrs, .03)
-    return pd.DataFrame(irr_array, columns=['IRR'], index=present_values.index)
-
-def compute_liab_ret(present_values, irr_df):
-    liab_ret = np.zeros(len(present_values))
-
-    for i in range (0,len(present_values)-1):
-        liab_ret[i+1] += irr_df['IRR'][i]/12 + ((present_values['Present Value'][i+1])/present_values['Present Value'][i])-1
-        
-    return pd.DataFrame(liab_ret, columns=['Liability'], index=present_values.index)
-    
-
-############################################################################################################################################################
-# IMPORT AND TRANSFORM CASHFLOW AND DISC RATE DATA                                                             
-############################################################################################################################################################
-PLAN = 'Total'
 df_pbo_cfs = dm.get_cf_data('PBO')
-df_pbo_cfs["Total"] = df_pbo_cfs["IBT"] + df_pbo_cfs["Retirement"] + df_pbo_cfs["Pension"]
-
-df_pvfb_cfs = dm.get_cf_data('PVFB')
-df_pvfb_cfs["Total"] =  df_pvfb_cfs["IBT"] + df_pvfb_cfs["Retirement"] + df_pvfb_cfs["Pension"]
-
 df_sc_cfs = dm.get_cf_data('Service Cost')
-df_ftse = dm.get_ftse_data(False)
+df_ftse = dm.get_ftse_data()
+plan_asset_data = dm.get_plan_asset_data()
+plan_mv_cfs_dict = dm.get_plan_mv_cfs_dict()
+
     
 ############################################################################################################################################################
 # TRANSFORM DATA TO LIABILITY MODEL INPUTS                                                           
 ############################################################################################################################################################
-pbo_cashflows = df_pbo_cfs[PLAN]
+plan = 'IBT'
+pbo_cashflows = df_pbo_cfs[plan]
 disc_factors = df_pbo_cfs['Time']
-sc_cashflows = df_sc_cfs[PLAN]
+sc_cashflows = df_sc_cfs[plan]
 liab_curve = dm.generate_liab_curve(df_ftse, pbo_cashflows)
-disc_rates = pd.read_excel(dm.TS_FP+"discount_rate_data_towers.xlsx",sheet_name=PLAN ,usecols=[0,1],index_col=0)
-asset_mv = dm.get_plan_data()
-contrb_pct = 0.00
-
-
-############################################################################################################################################################
-# COMPUTE PV, IRR, LIAB RET USING CURVE AND DISC RATES                                                         
-############################################################################################################################################################
-pv_curve = compute_pvs(pbo_cashflows, disc_factors, liab_curve)
-irr_curve = compute_irr(pv_curve, pbo_cashflows, disc_factors)
-liab_ret_curve = compute_liab_ret(pv_curve, irr_curve)
-
-#using discount rates *ignore*
-#pv_disc_rates = compute_pvs(pbo_cashflows, disc_factors, disc_rates=disc_rates)
-#irr_disc_rates = compute_irr(pv_disc_rates, pbo_cashflows, disc_factors)
-#liab_ret_disc_rates = compute_liab_ret(pv_disc_rates, irr_disc_rates)
-
+contrb_pct=.05
+asset_mv = dm.get_plan_asset_mv(plan_asset_data, plan)
+liab_mv_cfs = dm.offset(plan_mv_cfs_dict[plan])
+asset_ret = dm.get_plan_asset_returns(plan_asset_data, plan)
 
 ############################################################################################################################################################
 # INITIALIZE LIAB MODEL USING CURVE AND DISC RATES
 ############################################################################################################################################################
-liab_model_curve = liabilityModel(pbo_cashflows, disc_factors, sc_cashflows, contrb_pct, asset_mv,liab_curve)
+liab_model = liabilityModel(pbo_cashflows, disc_factors, sc_cashflows, contrb_pct, asset_mv, liab_mv_cfs,asset_ret, liab_curve)
 
-liab_model_disc_rates = liabilityModel(pbo_cashflows, disc_factors, sc_cashflows, contrb_pct, asset_mv,disc_rates=disc_rates)
+############################################################################################################################################################
+# GET LIABILITY DATA DICT THAT CONTAINS PLAN DATA (ASSET&LIABILITY MVS & RETURNS, IRR, PVS, FUNDED STATUS)
+############################################################################################################################################################
+liab_data_dict = liab_model.get_liab_data_dict(pbo_cashflows, sc_cashflows)
