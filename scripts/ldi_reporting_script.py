773b85751782d2457ddb848185021a7ff83a329f
pforjoeUPS <pforjoe@ups.com>
2023-01-21 06:50:53 -0500

    Update ldi_reporting_script.py

--- scripts/ldi_reporting_script.py
+++ scripts/ldi_reporting_script.py
@@ -18,14 +18,6 @@ report_dict = summary.get_report_dict()
 
 # end = time.time()
 #print(end - start)
-# temp_dict = {}
-# for key in liab_model_dict:
-#     temp_df = dm.merge_dfs(liab_model_dict[key]['Present Values'], liab_model_dict[key]['IRR'])
-#     temp_dict[key] = temp_df
-
-# report_dict['pv_irr_dict'] = temp_dict
-
-#rp.get_liability_returns_report(report_dict,report_name = "test")
 
 #new ldi report format
 rp.get_ldi_report(report_dict)
