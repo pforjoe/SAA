9845de9af3d99360e85af7f333db7957bac53282
mchoii <maddiechoi839@gmail.com>
2023-03-15 16:10:23 -0400

    Format plots on LDI report

--- AssetAllocation/reporting/plots.py
+++ AssetAllocation/reporting/plots.py
@@ -247,13 +247,24 @@ def get_asset_liab_ret_bar_plot(workbook, worksheet, sheet_name, ret_row_dim, po
     #set x axis
     returns_chart.set_x_axis({'label_position' : 'low',
                         'date_axis': True,
-                       'num_format' : 'mm-yy',})
+                       'num_format' : 'mmm-yy',
+                       'num_font':{'rotation':-45,'name': 'Arial','color':'#616161 '},})
     
     #set y axis format
-    returns_chart.set_y_axis({'num_format':'0.00%'})
+    returns_chart.set_y_axis({'num_format':'0%',
+                             'num_font':  {'name': 'Arial', 'color':'#616161 '},
+                             'line':{'none':True},
+                            'major_gridlines': {
+                                'visible' : 1,
+                                'line' : { 'color' : '#D3D3D3'}
+                                }
+                            })
     
     #set chart title
-    returns_chart.set_title({'name':sheet_name + " Plan - FTSE Curve"})
+    returns_chart.set_title({'name':sheet_name + " Plan - FTSE Curve" ,
+                             'name_font':  {'name': 'Arial','color':'#616161 ','bold':False}})
+    
+    returns_chart.set_chartarea({'border':{'none':True}})
     
     #set legend position
     returns_chart.set_legend({'position':'bottom'})
@@ -277,8 +288,8 @@ def get_fs_chart(workbook, worksheet, sheet_name, fs_row_dim, fs_col_dim, positi
     #set x axis
     fs_chart.set_x_axis({
                        'date_axis': True,
-                     'num_format' : 'mm/dd/yyyy',
-                     'num_font':{'rotation':-45},
+                     'num_format' : 'mmm-yy',
+                     'num_font':{'rotation':-45,'name': 'Arial','color':'#616161 '},
                      'minor_unit':1,
                      'minor_unit_type': 'days',
                      'major_unit':      1,
@@ -287,10 +298,20 @@ def get_fs_chart(workbook, worksheet, sheet_name, fs_row_dim, fs_col_dim, positi
                        })
     
     #set y axis format
-    fs_chart.set_y_axis({'num_format':'0.00%'})
+    fs_chart.set_y_axis({'num_format':'0%',
+                             'num_font':  {'name': 'Arial', 'color':'#616161 '},
+                             'line':{'none':True},
+                            'major_gridlines': {
+                                'visible' : 1,
+                                'line' : { 'color' : '#D3D3D3'}
+                                }
+                            })
     
     #set chart title
-    fs_chart.set_title({'name':"Funded Status - " + sheet_name})
+    fs_chart.set_title({'name':"Funded Status - " + sheet_name,
+                            'name_font':  {'name': 'Arial','color':'#616161 ','bold':False}})
+    
+    fs_chart.set_chartarea({'border':{'none':True}})
     
     fs_chart.set_legend({'position': 'none'})
 
@@ -311,7 +332,7 @@ def get_fs_vol_chart(workbook, worksheet, sheet_name, fs_row_dim, fs_col_dim, po
     #set x axis
     fs_vol_chart.set_x_axis({
                        'date_axis': True,
-                     'num_format' : 'mm/dd/yyyy',
+                     'num_format' : 'mmm-yy',
                      'num_font':{'rotation':-45,'name': 'Arial','color':'#616161 '},
                      'minor_unit':1,
                      'minor_unit_type': 'days',
@@ -322,7 +343,7 @@ def get_fs_vol_chart(workbook, worksheet, sheet_name, fs_row_dim, fs_col_dim, po
                        })
     
     #set y axis format
-    fs_vol_chart.set_y_axis({'num_format':'0.00%',
+    fs_vol_chart.set_y_axis({'num_format':'0%',
                              'num_font':  {'name': 'Arial', 'color':'#616161 '},
                              'line':{'none':True},
                             'major_gridlines': {
@@ -335,7 +356,7 @@ def get_fs_vol_chart(workbook, worksheet, sheet_name, fs_row_dim, fs_col_dim, po
     fs_vol_chart.set_title({'name':"Realized Funded Status Volatility",
                             'name_font':  {'name': 'Arial','color':'#616161 ','bold':False}
                             })
-    
+    fs_vol_chart.set_chartarea({'border':{'none':True}})
     fs_vol_chart.set_legend({'position': 'bottom',
                              'font': {'name': 'Arial','color':'#616161 '}
                                       })
