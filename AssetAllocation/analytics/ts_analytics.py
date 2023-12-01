e568233b1af5735b5204f67ff8d434086b6265a5
mchoii <maddiechoi839@gmail.com>
2023-12-01 12:11:30 -0500

    add npv and irr to ts_analytics

--- AssetAllocation/analytics/ts_analytics.py
+++ AssetAllocation/analytics/ts_analytics.py
@@ -7,6 +7,7 @@ Created on Fri Oct  1 17:05:16 2021
 
 import pandas as pd
 import numpy as np
+from scipy.optimize import fsolve
 from ..datamanager import datamanager as dm
 from .util import add_sharpe_col
 
@@ -158,4 +159,47 @@ def get_ret_vol_df(returns_df):
     ret_vol_df = pd.DataFrame(ret_vol_dict, index = ['Return', 'Volatility']).transpose()
     return add_sharpe_col(ret_vol_df)
 
- 
+def npv(irr, cfs, yrs):
+    """
+    Returns net present value of cashflows given an irr
+
+    Parameters
+    ----------
+    irr : double
+        IRR.
+    cfs : array
+        cashflows.
+    yrs : array
+        periods.
+
+    Returns
+    -------
+    double
+
+    """
+    return np.sum(cfs / (1. + irr) ** yrs)
+
+   
+def irr(cfs, yrs, x0, **kwargs):
+    """
+    Compute internal rate of return(IRR)
+
+    Parameters
+    ----------
+    cfs : array
+        cashflows.
+    yrs : array
+        periods.
+    x0 : double
+        guess.
+    
+    Returns
+    -------
+    double
+        IRR.
+
+    """
+    return np.ndarray.item(fsolve(npv, x0=x0,args=(cfs,yrs), **kwargs))
+
+
+    
